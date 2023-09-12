0777d71e56bedc89ef8aeb80bac4a9cdd6441285
Brooke Smith <brooke@oehmsmith.com>
2023-09-12 12:51:20 +1000

    Add transactions table

--- src/TransactionsTable.js
+++ src/TransactionsTable.js
@@ -1,10 +1,240 @@
-import React from "react"
+import React, { useContext } from "react"
+import { createColumnHelper, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table"
+import { DispatchContext, StateContext } from "./AppContext"
+import { BlocksTableRender } from "./BlocksTable"
 
-export function TransactionsTable() {
+export function TransactionsTableRender({ columns, data }) {
+    const state = useContext(StateContext);
+    const dispatch = useContext(DispatchContext);
+
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+    })
+
+    const cellAlignment = (id) => {
+        const items = id.split('_');
+        const name = items[items.length - 1]
+        // console.log(`cellAlignment: ${name}`)
+        const aligns = {
+            'gasLimit': 'right',
+            'gasUsed': 'right'
+        }
+        switch (name) {
+            case 'gasLimit':
+            case 'gasPrice':
+                return 'Align-' + aligns[name]
+            default:
+                return 'Align-left'
+        }
+    }
+
+    const hasClickHandler = id => {
+        const items = id.split('_');
+        const name = items[items.length - 1]
+        switch (name) {
+            case 'transactions':
+                return true
+            default:
+                return false
+        }
+    }
+    const onClickHandler = (cell) => {
+        const items = cell.id.split('_');
+        const name = items[items.length - 1]
+        // console.log(`onClickHandler: ${name}`)
+        switch (name) {
+            case 'transactions':
+                // console.log(` inside transactions - ${cell.getValue().length}`)
+                dispatch({type: 'setBlockTransactions', payload: cell.getValue()})
+            default:
+                return undefined
+        }
+    }
+    /*
+      Render the UI for your table
+      - react-table doesn't have UI, it's headless. We just need to put the react-table props from the Hooks, and it will do its magic automatically
+    */
+    return (
+        <div className="p-2">
+            <table>
+                <thead>
+                    {table.getHeaderGroups().map(headerGroup => (
+                        <tr key={headerGroup.id}>
+                            {headerGroup.headers.map(header => (
+                                <th key={header.id}>
+                                    {header.isPlaceholder
+                                        ? null
+                                        : flexRender(
+                                            header.column.columnDef.header,
+                                            header.getContext()
+                                        )}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody>
+                    {table.getRowModel().rows.map(row => (
+                        <tr key={row.id}>
+                            {row.getVisibleCells().map(cell => {
+                                if (hasClickHandler(cell.id)) {
+                                    return (
+                                        <td key={cell.id} className={cellAlignment(cell.id)}>
+                                            <button onClick={() => onClickHandler(cell)}>
+                                                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                            </button>
+                                        </td>
+                                    )
+                                } else {
+                                    return (
+                                        <td key={cell.id} className={cellAlignment(cell.id)}>
+                                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                        </td>
+                                    )
+
+                                }
+                            })}
+                        </tr>
+                    ))}
+                </tbody>
+                <tfoot>
+                    {table.getFooterGroups().map(footerGroup => (
+                        <tr key={footerGroup.id}>
+                            {footerGroup.headers.map(header => (
+                                <th key={header.id}>
+                                    {header.isPlaceholder
+                                        ? null
+                                        : flexRender(
+                                            header.column.columnDef.footer,
+                                            header.getContext()
+                                        )}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </tfoot>
+            </table>
+            <div className="h-4"/>
+        </div>
+    )
+}
+
+export function TransactionsTable({transactions}) {
+
+    const succinctise = input => {
+        if (!input) {
+            return ''
+        }
+        return input.length < 11 ? input : input.substring(0, 6) + "..." + input.substring(input.length - 6)
+    }
+
+    const age = timestamp => {
+        const floor = val => Math.floor(val);
+        const floorOut = val => floor(val).toString(10)
+
+        const now = new Date().getTime()
+        const ms = (now - (timestamp * 1000))
+        const secs = ms / 1000
+        const mins = secs / 60
+        const hours = mins / 60
+        const days = hours / 24
+        const months = days / 30.44
+        const years = days / 365.24
+        const monthsDiff = months - floor(years) * 12
+        const daysDiff = days - floor(months) * 30.44
+        const hoursDiff = hours - floor(days) * 24
+        const minsDiff = mins - floor(hours) * 60
+        const secsDiff = secs - floor(mins) * 60
+
+        const output = [];
+        const whatDone = new Map()
+
+        if (years > 1) {
+            output.push(floorOut(years) + ' years')
+            whatDone.set('years', true)
+        }
+        if (monthsDiff > 1) {
+            output.push(floorOut(monthsDiff) + ' months')
+            whatDone.set('months', true)
+        }
+        if (daysDiff > 1) {
+            output.push(floorOut(daysDiff) + ' days')
+            whatDone.set('days', true)
+        }
+        if (hoursDiff > 1) {
+            output.push(floorOut(hoursDiff) + ' hours')
+            whatDone.set('hours', true)
+        }
+        if (minsDiff > 1) { //} && whatDone.size === 0) {
+            output.push(floorOut(minsDiff) + ' mins')
+            whatDone.set('mins', true)
+        }
+        if (secsDiff > 1) { // && whatDone.size === 0) {
+            output.push(floorOut(secsDiff) + ' secs')
+            whatDone.set('secs', true)
+        }
+
+        // console.log(`x timestamp: ${timestamp} - now: ${now}, ms: ${ms}, sec: ${secs}, secsDiff: ${secsDiff}, min: ${mins}, minsDiff: ${minsDiff}, hours: ${hours} - ${output.join(', ')}`)
+        return output.join(' ')
+    }
+
+    const format = number => new Intl.NumberFormat().format(number)
+
+    const columnHelper = createColumnHelper()
+
+    const columns = [
+        columnHelper.accessor('hash', {
+            cell: info => info.getValue(),
+            // footer: info => info.column.id,
+        }),
+        columnHelper.accessor('blockNumber', {
+            cell: info => format(info.getValue()),
+            header: () => <span>block<br/>number</span>,
+            // footer: info => info.column.id,
+        }),
+        columnHelper.accessor('timestamp', {
+            header: () => 'age',
+            cell: info => age(info.getValue()),
+            // footer: info => info.column.id,
+        }),
+        columnHelper.accessor('gasLimit', {
+            header: () => <span>gasLimit</span>,
+            cell: info => format(info.getValue())
+            // footer: info => info.column.id,
+        }),
+        columnHelper.accessor('gasPrice', {
+            header: 'gasPrice',
+            cell: info => format(info.getValue())
+            // footer: info => info.column.id,
+        }),
+        columnHelper.accessor('from', {
+            header: 'from',
+            cell: info => succinctise(info.getValue()),
+            // footer: info => info.column.id,
+        }),
+        columnHelper.accessor('to', {
+            header: 'to',
+            cell: info => succinctise(info.getValue()),
+            // footer: info => info.column.id,
+        }),
+        columnHelper.accessor('value', {
+            header: 'value',
+            cell: info => info.getValue()._hex
+            // {
+            //     return (
+            //         <span onClick={console.log(`yep: ` + info.getValue().length)}>Frog: {info.getValue()?.length}</span>
+            //     )
+            // },
+            // footer: info => info.column.id,
+
+        }),
+    ]
 
     return (
         <>
-            <h4>transactions</h4>
+            <TransactionsTableRender columns={columns} data={transactions}/>
         </>
     )
 }
